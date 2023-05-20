3f1c6d0c20d46d32ca7f832eb39c37f372b8cb83
I-Slot <ismailadesholaharun@gmail.com>
2023-05-20 19:00:51 +0100

    update

--- Random Number Generator/index.js
+++ Random Number Generator/index.js
@@ -4,6 +4,6 @@ let randomNumberEl = document.querySelector(".random-number")
 let generateNumBtn = document.querySelector(".generate-num");
 
 generateNumBtn.addEventListener("click", () => {
-    randomNumberEl.textContent = Math.trunc((Math.random()) * 20)
+    randomNumberEl.textContent = Math.trunc((Math.random()) * 1000)
     console.log(randomNumberEl.textContent)
 })
