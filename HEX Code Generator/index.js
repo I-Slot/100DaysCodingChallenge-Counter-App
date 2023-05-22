cbeda93dc84fcd3f861dbd1633d804e16b22eaac
I-Slot <ismailadesholaharun@gmail.com>
2023-05-22 17:34:55 +0100

    popup-message

--- HEX Code Generator/index.js
+++ HEX Code Generator/index.js
@@ -1,1 +1,20 @@
 "strict"
+
+let buttonEl = document.querySelector(".generate-color");
+let colorEl = document.querySelector(".color");
+
+const generateColor = () =>{
+    let color = Math.random().toString(16).substring(2, 8);
+    color = `#${color}`;
+    colorEl.innerHTML = color
+    document.body.style.backgroundColor = color
+}
+
+// generateColor()
+
+
+buttonEl.addEventListener("click",generateColor)
+
+
+// Sub-string Method
+// console.log(color);
