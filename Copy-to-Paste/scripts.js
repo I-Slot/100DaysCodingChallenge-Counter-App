d8d8977ce65e52b3fc406e6631d4d974a5db86d2
I-Slot <ismailadesholaharun@gmail.com>
2023-06-09 18:15:59 +0100

    update on using navitor API to render the copy and paste functionality

--- Copy-to-Paste/scripts.js
+++ Copy-to-Paste/scripts.js
@@ -4,15 +4,20 @@ const coupon = document.querySelector(".coupon");
 const copyText = (e) => {
   e.preventDefault();
 
-  coupon.select();
-  coupon.setSelectionRange(0, 999999);
-  document.execCommand("copy");
-
+  // coupon.select();
+  // coupon.setSelectionRange(0, 999999);
+  // document.execCommand("copy");
+  navigator.clipboard.writeText(coupon.value).then(() => {
     btn.textContent = "Copied!!!";
+  
+    setTimeout(() => {
+      btn.textContent = "Copy";
+    }, 3000);
+    });
+}
     
-  setTimeout(() => {
-    btn.textContent = "Copy";
-  }, 3000);
-};
 
 btn.addEventListener("click", copyText);
+
+
+
